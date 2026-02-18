c15812c6d12c7fbd8085054c3b4195a30579ccf9
SebastianM <jhoan.moreno@udea.edu.co>
2026-02-17 20:52:17 -0500

    entorno_virtual

---
+++ entorno_virtual.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as ps
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+
+# streamlit run index.py para corre en web - se debe escribir en la terminal - 
+# y al hacer ctrl + C sale de streamlit
